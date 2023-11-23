cb17ea3938037a9ea7904729119af4dd3de9c385
jisungbin <ji@sungb.in>
2023-11-24 02:34:09 +0900

    Added support for the DeclarationStability extension

--- runtime/src/main/kotlin/land/sungbin/composeinvestigator/runtime/DeclarationStability.kt
+++ runtime/src/main/kotlin/land/sungbin/composeinvestigator/runtime/DeclarationStability.kt
@@ -5,13 +5,13 @@
  * Please see full license: https://github.com/jisungbin/ComposeInvestigator/blob/main/LICENSE
  */
 
+// This code is based on https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/analysis/Stability.kt;l=74;drc=7d3e127599f8ce142445c944c703cf3c3e6d2b3a.
+
 @file:Suppress("MemberVisibilityCanBePrivate")
 
 package land.sungbin.composeinvestigator.runtime
 
-// Get from https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/analysis/Stability.kt;l=74;drc=7d3e127599f8ce142445c944c703cf3c3e6d2b3a.
 // The name is prefixed with "Declaration" to distinguish it from AOSP's Stability.
-
 public sealed class DeclarationStability {
   public class Certain(public val stable: Boolean) : DeclarationStability() {
     override fun toString(): String = if (stable) "Stable" else "Unstable"
@@ -33,3 +33,41 @@ public sealed class DeclarationStability {
     override fun toString(): String = elements.joinToString(",")
   }
 }
+
+public fun DeclarationStability.toCertainString(): String = if (knownStable()) "Stable" else "Unstable"
+
+public fun DeclarationStability.knownStable(): Boolean =
+  when (this) {
+    is DeclarationStability.Certain -> stable
+    is DeclarationStability.Runtime -> false
+    is DeclarationStability.Unknown -> false
+    is DeclarationStability.Parameter -> false
+    is DeclarationStability.Combined -> elements.all(DeclarationStability::knownStable)
+  }
+
+public fun DeclarationStability.knownUnstable(): Boolean =
+  when (this) {
+    is DeclarationStability.Certain -> !stable
+    is DeclarationStability.Runtime -> false
+    is DeclarationStability.Unknown -> false
+    is DeclarationStability.Parameter -> false
+    is DeclarationStability.Combined -> elements.any(DeclarationStability::knownUnstable)
+  }
+
+public fun DeclarationStability.isUncertain(): Boolean =
+  when (this) {
+    is DeclarationStability.Certain -> false
+    is DeclarationStability.Runtime -> true
+    is DeclarationStability.Unknown -> true
+    is DeclarationStability.Parameter -> true
+    is DeclarationStability.Combined -> elements.any(DeclarationStability::isUncertain)
+  }
+
+public fun DeclarationStability.isExpressible(): Boolean =
+  when (this) {
+    is DeclarationStability.Certain -> true
+    is DeclarationStability.Runtime -> true
+    is DeclarationStability.Unknown -> false
+    is DeclarationStability.Parameter -> true
+    is DeclarationStability.Combined -> elements.all(DeclarationStability::isExpressible)
+  }
