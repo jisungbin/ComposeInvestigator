37e62cdfa07ca77952cc2b9080a046a3c292ebc8
jisungbin <ji@sungb.in>
2023-11-24 02:37:15 +0900

    Composable invalidation logger improvements
    - Support for more formats
    - Improved code structure

--- compiler/src/main/kotlin/land/sungbin/composeinvestigator/compiler/internal/tracker/logger/InvalidationLoggerVisitor.kt
+++ compiler/src/main/kotlin/land/sungbin/composeinvestigator/compiler/internal/tracker/logger/InvalidationLoggerVisitor.kt
@@ -5,12 +5,12 @@
  * Please see full license: https://github.com/jisungbin/ComposeInvestigator/blob/main/LICENSE
  */
 
-package land.sungbin.composeinvestigator.compiler.internal.logger
+package land.sungbin.composeinvestigator.compiler.internal.tracker.logger
 
 import land.sungbin.composeinvestigator.compiler.internal.AFFECTED_COMPOSABLE_FQN
 import land.sungbin.composeinvestigator.compiler.internal.COMPOSABLE_FQN
-import land.sungbin.composeinvestigator.compiler.internal.COMPOSABLE_INVALIDATE_TYPE_FQN
-import land.sungbin.composeinvestigator.compiler.internal.COMPOSE_INVESTIGATE_LOGGER_FQN
+import land.sungbin.composeinvestigator.compiler.internal.COMPOSABLE_INVALIDATION_LOGGER_FQN
+import land.sungbin.composeinvestigator.compiler.internal.COMPOSABLE_INVALIDATION_TYPE_FQN
 import land.sungbin.composeinvestigator.compiler.util.VerboseLogger
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.IrStatement
@@ -25,23 +25,23 @@ import org.jetbrains.kotlin.ir.util.isTopLevel
 import org.jetbrains.kotlin.ir.util.kotlinFqName
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
-internal class InvestigateLoggerVisitor(
+internal class InvalidationLoggerVisitor(
   private val context: IrPluginContext,
   private val logger: VerboseLogger,
 ) : IrElementTransformerVoid(), IrPluginContext by context {
   override fun visitSimpleFunction(declaration: IrSimpleFunction): IrStatement {
-    val currentLoggerSymbol = InvestigateLogger.getCurrentLoggerSymbolOrNull()
-    if (declaration.isValidComposeInvestigateLoggerFunction()) {
+    val currentLoggerSymbol = InvalidationLogger.getCurrentLoggerSymbolOrNull()
+    if (declaration.isValidComposableInvalidationLoggerFunction()) {
       if (currentLoggerSymbol != null && currentLoggerSymbol.owner.kotlinFqName != declaration.kotlinFqName) {
         val message = """
-          More than one @ComposeInvestigateLogger detected. To avoid confusion, the default logger is used.
+          More than one @ComposableInvalidationLogger detected. To avoid confusion, the default logger is used.
           Detected loggers: [${currentLoggerSymbol.owner.kotlinFqName.asString()}, ${declaration.kotlinFqName.asString()}]
         """.trimIndent()
         // TODO: print message with warning level
         logger(message)
-        InvestigateLogger.useDefaultLogger(context)
+        InvalidationLogger.useDefaultLogger(context)
       } else if (currentLoggerSymbol == null) {
-        InvestigateLogger.useCustomLogger(declaration.symbol)
+        InvalidationLogger.useCustomLogger(declaration.symbol)
         logger("Found ComposeInvestigateLogger function: ${declaration.dump()}")
       }
     }
@@ -54,9 +54,9 @@ internal class InvestigateLoggerVisitor(
   // 4. no extension receiver and no context receiver
   // 5. not suspend
   // 6. unit type
-  // 7. has only two parameters: <AffectedComposable, ComposableInvalidateType>
-  private fun IrFunction.isValidComposeInvestigateLoggerFunction(): Boolean =
-    hasAnnotation(COMPOSE_INVESTIGATE_LOGGER_FQN) &&
+  // 7. has only two parameters: <AffectedComposable, ComposableInvalidationType>
+  private fun IrFunction.isValidComposableInvalidationLoggerFunction(): Boolean =
+    hasAnnotation(COMPOSABLE_INVALIDATION_LOGGER_FQN) &&
       !hasAnnotation(COMPOSABLE_FQN) &&
       isTopLevel &&
       visibility.isPublicAPI &&
@@ -66,5 +66,5 @@ internal class InvestigateLoggerVisitor(
       returnType.isUnit() &&
       valueParameters.size == 2 &&
       valueParameters[0].type.classFqName == AFFECTED_COMPOSABLE_FQN &&
-      valueParameters[1].type.classFqName == COMPOSABLE_INVALIDATE_TYPE_FQN
+      valueParameters[1].type.classFqName == COMPOSABLE_INVALIDATION_TYPE_FQN
 }
---
+++ runtime/src/main/kotlin/land/sungbin/composeinvestigator/runtime/ComposableInvalidationLogger.kt
@@ -0,0 +1,86 @@
+/*
+ * Designed and developed by Ji Sungbin 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/jisungbin/ComposeInvestigator/blob/main/LICENSE
+ */
+
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+package land.sungbin.composeinvestigator.runtime
+
+/**
+ * ```
+ * @ComposableInvalidationLogger
+ * public fun invalidationLogger(composable: AffectedComposable, type: ComposableInvalidationType) {
+ *   // Your logger code here
+ * }
+ * ```
+ */
+@MustBeDocumented
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.FUNCTION)
+public annotation class ComposableInvalidationLogger
+
+public data class AffectedComposable(
+  public val name: String,
+  public val pkg: String,
+  public val filePath: String,
+  @Deprecated("This value is not yet provided correctly.") public val startLine: Int,
+  @Deprecated("This value is not yet provided correctly.") public val startColumn: Int,
+)
+
+public data class Parameter(
+  public val name: String,
+  public val stability: DeclarationStability,
+)
+
+public data class ParameterInfo(
+  public val name: String,
+  public val valueString: String,
+  public val valueHashCode: Int,
+  public val stability: DeclarationStability,
+)
+
+public sealed class InvalidationReason {
+  public data object Initial : InvalidationReason() {
+    override fun toString(): String = "Initial composition for building data inside Compose."
+  }
+
+  public data class ParameterChanged(public val changedParams: List<Pair<ParameterInfo, ParameterInfo>>) : InvalidationReason() {
+    override fun toString(): String = buildString {
+      appendLine("ChangedParams(")
+      for ((index, changedParam) in changedParams.withIndex()) {
+        val (prevParam, newParam) = changedParam
+        require(prevParam.name == newParam.name) {
+          "Parameter name must be same. prevParam.name=${prevParam.name}, newParam.name=${newParam.name}"
+        }
+        val message =
+          "${index + 1}. [${prevParam.name} <${prevParam.stability.toCertainString()}>] " +
+            "${prevParam.valueString} (${prevParam.valueHashCode}) -> ${newParam.valueString} (${newParam.valueHashCode})"
+        appendLine("  $message")
+      }
+      appendLine(")")
+    }
+  }
+
+  // According to the Compose compiler's comments, this should be determinable via the $changed argument.
+  // https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt;l=381-382;drc=ea884612191a32933b697cc5062aa32505be4eaa
+  // However, I haven't yet figured out how to determine this, so this type is not used. (in TODO status)
+  @Deprecated("Force is not supported yet.")
+  public data object Force : InvalidationReason() {
+    override fun toString(): String = "A forced recomposition has been requested for the current composable."
+  }
+
+  public data class Unknown(public val params: List<Parameter>) : InvalidationReason() {
+    override fun toString(): String = """
+      No diff params. Some parameter may be unstable.
+      ParameterInfos: ${params.joinToString()}
+    """.trimIndent()
+  }
+}
+
+public sealed class ComposableInvalidationType {
+  public data class Processed(public val reason: InvalidationReason) : ComposableInvalidationType()
+  public data object Skipped : ComposableInvalidationType()
+}
