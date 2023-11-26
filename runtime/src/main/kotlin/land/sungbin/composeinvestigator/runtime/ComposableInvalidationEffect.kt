c9d3966abca6f92a8867b61606e68a420bbb8c3d
jisungbin <ji@sungb.in>
2023-11-26 18:21:53 +0900

    ComposableInvalidationEffect is TODO

--- runtime/src/main/kotlin/land/sungbin/composeinvestigator/runtime/ComposableInvalidationEffect.kt
+++ runtime/src/main/kotlin/land/sungbin/composeinvestigator/runtime/ComposableInvalidationEffect.kt
@@ -7,13 +7,14 @@
 
 // This code is based on https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/Effects.kt;l=281;drc=8fa982f966a52c0b72cdf4756461354914a178be.
 
-@file:Suppress("UNUSED_PARAMETER")
-
 package land.sungbin.composeinvestigator.runtime
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.InternalComposeApi
 import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.RememberObserver
+import androidx.compose.runtime.currentComposer
+import androidx.compose.runtime.remember
 import java.util.concurrent.CancellationException
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
@@ -21,47 +22,30 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
-@Composable
-@NonRestartableComposable
-public fun ComposableInvalidationEffect(
-  key1: Any?,
-  block: suspend CoroutineScope.(invalidationTrackTable: ComposableInvalidationTrackTable) -> ComposableInvalidationListener,
-) {
-  throw NotImplementedError("Implemented as an intrinsic")
-}
-
-@Composable
-@NonRestartableComposable
-public fun ComposableInvalidationEffect(
-  key1: Any?,
-  key2: Any?,
-  block: suspend CoroutineScope.(invalidationTrackTable: ComposableInvalidationTrackTable) -> ComposableInvalidationListener,
-) {
-  throw NotImplementedError("Implemented as an intrinsic")
-}
-
-@Composable
-@NonRestartableComposable
-public fun ComposableInvalidationEffect(
-  key1: Any?,
-  key2: Any?,
-  key3: Any?,
-  block: suspend CoroutineScope.(invalidationTrackTable: ComposableInvalidationTrackTable) -> ComposableInvalidationListener,
-) {
-  throw NotImplementedError("Implemented as an intrinsic")
-}
-
+// TODO: `invalidationTrackTable.currentComposableKeyName`의 값이 ComposableInvalidationEffect 함수 자체의 키로
+//  적용되어서 예상하지 않은 결과로 이어짐. 이를 해결하기 위해선 ComposableInvalidationEffect 함수도 intrinsic으로
+//  만들어야 할 거 같음.
+@OptIn(InternalComposeApi::class)
 @Composable
 @NonRestartableComposable
 public fun ComposableInvalidationEffect(
   vararg keys: Any?,
   block: suspend CoroutineScope.(invalidationTrackTable: ComposableInvalidationTrackTable) -> ComposableInvalidationListener,
 ) {
-  throw NotImplementedError("Implemented as an intrinsic")
+  val applyContext = currentComposer.applyCoroutineContext
+  val invalidationTrackTable = currentComposableInvalidationTracker
+  val composableKey = invalidationTrackTable.currentComposableKeyName
+  remember(*keys) {
+    InvalidationEffectScope(
+      parentCoroutineContext = applyContext,
+      composableKey = composableKey,
+      invalidationTrackTable = invalidationTrackTable,
+      task = block,
+    )
+  }
 }
 
-@ComposeInvestigatorCompilerApi
-public class InvalidationEffectScope(
+private class InvalidationEffectScope(
   parentCoroutineContext: CoroutineContext,
   private val composableKey: String,
   private val invalidationTrackTable: ComposableInvalidationTrackTable,
