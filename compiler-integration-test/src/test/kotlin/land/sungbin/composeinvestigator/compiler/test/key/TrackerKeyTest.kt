0a08149db756170967d3456de71f59af6dbc01a0
jisungbin <ji@sungb.in>
2024-01-15 19:58:25 +0900

    Remove duplicates header

--- compiler-integration-test/src/test/kotlin/land/sungbin/composeinvestigator/compiler/test/key/TrackerKeyTest.kt
+++ compiler-integration-test/src/test/kotlin/land/sungbin/composeinvestigator/compiler/test/key/TrackerKeyTest.kt
@@ -5,13 +5,6 @@
  * Please see full license: https://github.com/jisungbin/ComposeInvestigator/blob/main/LICENSE
  */
 
-/*
- * Designed and developed by Ji Sungbin 2023.
- *
- * Licensed under the MIT.
- * Please see full license: https://github.com/jisungbin/ComposeInvestigator/blob/main/LICENSE
- */
-
 package land.sungbin.composeinvestigator.compiler.test.key
 
 import androidx.compose.compiler.plugins.kotlin.WeakBindingTrace
