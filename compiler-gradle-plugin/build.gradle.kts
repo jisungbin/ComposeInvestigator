c319eda415c75cef7fad513b590ef5c072b23777
jisungbin <ji@sungb.in>
2024-02-29 16:00:55 +0900

    Remove module version hardcodes

--- compiler-gradle-plugin/build.gradle.kts
+++ compiler-gradle-plugin/build.gradle.kts
@@ -14,6 +14,30 @@ plugins {
   id(libs.plugins.gradle.publish.maven.get().pluginId)
 }
 
+// Thanks for https://github.com/ZacSweers/redacted-compiler-plugin/blob/c866a8ae7b2ab039fee9709c990a5478ac0dc0c7/redacted-compiler-plugin-gradle/build.gradle.kts#L21-L34
+sourceSets {
+  main {
+    java.srcDir(layout.buildDirectory.dir("generated/sources/version-template/kotlin/main"))
+  }
+}
+
+val copyVersionTemplateProvider =
+  tasks.register<Copy>("copyVersionTemplate") {
+    inputs.property("version", project.property("VERSION_NAME"))
+    from(project.layout.projectDirectory.dir("version-template"))
+    into(project.layout.buildDirectory.dir("generated/sources/version-template/kotlin/main"))
+    expand(mapOf("projectVersion" to "${project.property("VERSION_NAME")}"))
+    filteringCharset = "UTF-8"
+  }
+
+tasks
+  .matching { task -> task.name == "sourcesJar" || task.name == "dokkaHtml" }
+  .configureEach { dependsOn(copyVersionTemplateProvider) }
+
+tasks.withType<KotlinCompile>().configureEach {
+  dependsOn(copyVersionTemplateProvider)
+}
+
 gradlePlugin {
   plugins {
     create("composeInvestigatorPlugin") {
@@ -30,9 +54,4 @@ kotlin {
 dependencies {
   compileOnly(libs.kotlin.gradle.core)
   compileOnly(libs.kotlin.gradle.api)
-}
-
-// configuration required to produce unique META-INF/*.kotlin_module file names
-tasks.withType<KotlinCompile>().configureEach {
-  kotlinOptions { moduleName = project.property("POM_ARTIFACT_ID") as String }
 }
