4909cfcd57188f5b77bd29ed22b5d56db9c88d57
Ji Sungbin <forky.ji@healingpaper.com>
2024-10-04 04:27:08 +0900

    Write BasicLayoutTest

--- compiler-integration-test/src/main/kotlin/land/sungbin/composeinvestigator/compiler/test/BasicLayout.kt
+++ compiler-integration-test/src/main/kotlin/land/sungbin/composeinvestigator/compiler/test/BasicLayout.kt
@@ -8,15 +8,18 @@
 package land.sungbin.composeinvestigator.compiler.test
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mock.Linear
 import androidx.compose.runtime.mock.Text
+import land.sungbin.composeinvestigator.runtime.ComposableInformation
+import land.sungbin.composeinvestigator.runtime.currentComposableInvalidationTracer
+
+val basicLayoutTable by lazy { currentComposableInvalidationTracer }
 
 @Composable fun BasicLayout() {
-  Text("Root")
-  Linear {
-    repeat(3) {
-      Text("Child $it")
-    }
-  }
-  Text("Tail")
+  Text("")
 }
+
+fun basicLayout() = ComposableInformation(
+  name = "BasicLayout",
+  packageName = "land.sungbin.composeinvestigator.compiler.test",
+  fileName = "BasicLayout.kt"
+)
--- compiler-integration-test/src/test/kotlin/land/sungbin/composeinvestigator/compiler/test/BasicLayoutTest.kt
+++ compiler-integration-test/src/test/kotlin/land/sungbin/composeinvestigator/compiler/test/BasicLayoutTest.kt
@@ -7,16 +7,55 @@
 
 package land.sungbin.composeinvestigator.compiler.test
 
+import androidx.compose.runtime.RecomposeScope
+import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.runtime.mock.compositionTest
+import androidx.compose.runtime.mock.expectNoChanges
+import assertk.assertThat
+import assertk.assertions.containsExactly
+import assertk.assertions.containsOnly
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import land.sungbin.composeinvestigator.compiler.test.TestConfiguration.logs
+import land.sungbin.composeinvestigator.runtime.InvalidationReason
+import land.sungbin.composeinvestigator.runtime.InvalidationType
 
 class BasicLayoutTest {
   @BeforeTest fun prepare() {
     TestConfiguration.reset()
+    basicLayoutTable.reset()
   }
 
-  @Test fun basic(): Unit = compositionTest {
+  @Test fun initialComposition() = compositionTest {
     compose { BasicLayout() }
+    assertThat(logs).containsOnly(
+      Investigated(
+        basicLayout(),
+        InvalidationType.Processed(InvalidationReason.Initial),
+      )
+    )
+  }
+
+  @Test fun skipRecomposition() = compositionTest {
+    var recomposeScope: RecomposeScope? = null
+
+    compose {
+      recomposeScope = currentRecomposeScope
+      BasicLayout()
+    }
+
+    recomposeScope!!.invalidate()
+    expectNoChanges()
+
+    assertThat(logs).containsExactly(
+      Investigated(
+        basicLayout(),
+        InvalidationType.Processed(InvalidationReason.Initial),
+      ),
+      Investigated(
+        basicLayout(),
+        InvalidationType.Skipped,
+      ),
+    )
   }
 }
