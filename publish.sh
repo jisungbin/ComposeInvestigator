9d1ab962aa047cfe6117afa7c453df1c07dd81cb
Ji Sungbin <forky.ji@healingpaper.com>
2024-10-20 01:32:55 +0900

    Prepare publishing

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+PROJECTS=(
+  ":runtime"
+  ":compiler"
+  ":compiler-gradle-plugin"
+)
+
+COMMAND_SUFFIX="publishMavenPublicationToMavenCentralRepository"
+
+if [ "$1" == "local" ]; then
+  COMMAND_SUFFIX="publishToMavenLocal"
+fi
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+for PROJECT in "${PROJECTS[@]}"; do
+  echo -e "${GREEN}Executing: ./gradlew ${PROJECT}:${COMMAND_SUFFIX}${NC}"
+  ./gradlew "${PROJECT}:${COMMAND_SUFFIX}" --console=plain
+done
