393c8939114fa24da5f963d10c9afbca950c0355
Ji Sungbin <ji@sungb.in>
2025-02-10 22:40:48 +0900

    internal test

--- compiler-gradle-plugin/src/main/kotlin/land/sungbin/composeinvestigator/gradle/VERSION.kt
+++ compiler-gradle-plugin/src/main/kotlin/land/sungbin/composeinvestigator/gradle/VERSION.kt
@@ -1,5 +1,5 @@
-// Copyright 2024 Ji Sungbin
+// Copyright 2025 Ji Sungbin
 // SPDX-License-Identifier: Apache-2.0
 package land.sungbin.composeinvestigator.gradle
 
-internal const val VERSION = "2.1.0-1.0.0.a3"
+internal const val VERSION = "2.1.0-1.0.0.a3-internal01"
