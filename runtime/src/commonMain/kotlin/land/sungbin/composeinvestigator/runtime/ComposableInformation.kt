7b519c152890921a50ba398b0d198cbfcea58873
Ji Sungbin <ji@sungb.in>
2025-03-10 08:16:00 +0900

    Clean up runtime APIs

--- runtime/src/commonMain/kotlin/land/sungbin/composeinvestigator/runtime/ComposableInformation.kt
+++ runtime/src/commonMain/kotlin/land/sungbin/composeinvestigator/runtime/ComposableInformation.kt
@@ -9,24 +9,19 @@ import androidx.compose.runtime.Stable
 /**
  * Indicates which Composables were affected by the state change.
  *
- * @param name Composable function name
+ * @param simpleName Composable function name
  * @param packageName Package name that the Composable function is defined
  * @param fileName File name that the Composable function is defined
  * @param compoundKey Same as [Composer.compoundKeyHash]. If no lookup
  * is possible, `null`.
  */
 @Immutable public data class ComposableInformation(
-  public val name: String,
-  public val packageName: String,
   public val fileName: String,
+  public val packageName: String,
+  public val simpleName: String,
   public val compoundKey: Int? = null,
-) {
-  /** @suppress ComposeInvestigator compiler-only API. Use `copy(compoundKey = N)` instead. */
-  @[ComposeInvestigatorCompilerApi Stable]
-  public fun withCompoundKey(compoundKey: Int): ComposableInformation =
-    copy(compoundKey = compoundKey)
-}
+)
 
 /** Fully-qualified name of the Composable function. */
-public val ComposableInformation.fqPackageName: String
-  @Stable get() = packageName.takeUnless(String::isEmpty)?.plus(".").orEmpty() + name
+public val ComposableInformation.qualifiedName: String
+  @Stable get() = packageName.takeUnless(String::isEmpty)?.plus(".").orEmpty() + simpleName
--- runtime/src/commonTest/kotlin/land/sungbin/composeinvestigator/runtime/ComposableInvalidationTraceTableTest.kt
+++ runtime/src/commonTest/kotlin/land/sungbin/composeinvestigator/runtime/ComposableInvalidationTraceTableTest.kt
@@ -3,14 +3,12 @@
 package land.sungbin.composeinvestigator.runtime
 
 import assertk.assertThat
-import assertk.assertions.containsOnly
 import assertk.assertions.isEqualTo
 import kotlin.test.Test
-import land.sungbin.composeinvestigator.runtime.ComposableInvalidationTraceTable.AffectedName
 
 class ComposableInvalidationTraceTableTest {
   @Test fun computeInvalidationReasonWhenNoPreviousInfo() {
-    val table = ComposableInvalidationTraceTable()
+    val table = ComposeInvestigator()
 
     val argument = ValueArgument(
       name = "name",
@@ -19,14 +17,13 @@ class ComposableInvalidationTraceTableTest {
       valueString = "value",
       valueHashCode = 0,
     )
-    val reason = table.computeInvalidationReason(keyName = "keyName", compoundKey = 0, arguments = listOf(argument))
+    val reason = table.computeInvalidationReason(compoundKey = 0, arguments = listOf(argument))
 
-    assertThat(table.affectedArguments).containsOnly(AffectedName("keyName", compoundKey = 0) to listOf(argument))
     assertThat(reason).isEqualTo(InvalidationResult.InitialComposition)
   }
 
   @Test fun computeInvalidationReasonWhenPreviousInfoExists() {
-    val table = ComposableInvalidationTraceTable()
+    val table = ComposeInvestigator()
 
     val oldArgument = ValueArgument(
       name = "name",
@@ -43,10 +40,9 @@ class ComposableInvalidationTraceTableTest {
       valueHashCode = 1,
     )
 
-    table.computeInvalidationReason(keyName = "keyName", compoundKey = 0, arguments = listOf(oldArgument))
-    val reason = table.computeInvalidationReason(keyName = "keyName", compoundKey = 0, arguments = listOf(newArgument))
+    table.computeInvalidationReason(compoundKey = 0, arguments = listOf(oldArgument))
+    val reason = table.computeInvalidationReason(compoundKey = 0, arguments = listOf(newArgument))
 
-    assertThat(table.affectedArguments).containsOnly(AffectedName("keyName", compoundKey = 0) to listOf(newArgument))
     assertThat(reason).isEqualTo(InvalidationResult.ArgumentChanged(listOf(oldArgument changedTo newArgument)))
   }
 
